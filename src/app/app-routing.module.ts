caf14a7a578ba7dd61a775511989d9bec7d706d7
RenanaH <renanashuv@gmail.com>
2019-05-26 21:39:20 +0300

    add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WhatThisCeliacComponent } from './what-this-celiac/what-this-celiac.component';
+import { AboutCeliacCommunityComponent } from './about-celiac-community/about-celiac-community.component';
+import { OrderProductsComponent } from './order-products/order-products.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-what-this-celiac', component: WhatThisCeliacComponent },
+  { path: 'app-about-celiac-community', component: AboutCeliacCommunityComponent },
+  { path: 'app-order-products', component: OrderProductsComponent },
+  { path: 'app-contact-us', component: ContactUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
